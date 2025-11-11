6a08ee3ed6b5a2d89d32a69a4e8eb41226942ee3
Swobenator <soheibtgf@gmail.com>
2025-11-11 13:22:12 +0000

    Integrated Crypto Module into Flask app
    Made use of Danny's crypto.py module to

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-
+from crypto import crypto
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 port = 3000
 
+public, private, primes = crypto.create_keys()
+VIG_KEY = "Bogdan"
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', private_key_d=private[0], private_key_n=private[1]                           )
 
 
 @socketio.on('send_message')
 def handle_send(data):
     msg = data.get('message', '')
 
+    vig_cipher, rsa_blocks = crypto.package_for_sender(msg, VIG_KEY, public)
+
     emit('receive_message', {
-        "message": msg,
+        "vig_ciphertext": vig_cipher,
+        "rsa_key_blocks": rsa_blocks
     }, broadcast=True)
 
 
